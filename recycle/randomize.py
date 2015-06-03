beeaa3e53a9f1b99a6045da805b32d08e071ad2c
Zoltan Szalai <flack@pionlab.com>
2015-06-03 19:52:06 +0200

    add random_date_in_range function

--- recycle/randomize.py
+++ recycle/randomize.py
@@ -1,7 +1,15 @@
 import random
+from datetime import timedelta
 
 from heapq import nlargest
 
 
 def sample_from_iterable(it, k):
     return (x for _, x in nlargest(k, ((random.random(), x) for x in it)))
+
+
+
+def random_date_in_range(start, end):
+    return start + timedelta(
+        seconds=random.randint(0, int((end - start).total_seconds()))
+        )
