b54061ba16fa597bf88ae910b733dec586e9017a
Zoltan Szalai <flack@mcule.com>
2020-01-20 18:40:16 +0100

    add xls_utils module

---
+++ recycle/xls_utils.py
@@ -0,0 +1,26 @@
+import xlrd
+
+
+def xls_reader(path, sheet_index=0):
+    wb = xlrd.open_workbook(path)
+    try:
+        sheet = wb.sheet_by_index(sheet_index)
+    except IndexError as e:
+        return []
+
+    row_idx = -1
+    while True:
+        row_idx += 1
+        try:
+            row = sheet.row_values(row_idx)
+        except IndexError as e:
+            break
+
+        yield row
+
+
+def xls_dict_reader(*args, **kwargs):
+    r = xls_reader(*args, **kwargs)
+    keys = next(r)
+    for row in r:
+        yield dict(zip(keys, row))
